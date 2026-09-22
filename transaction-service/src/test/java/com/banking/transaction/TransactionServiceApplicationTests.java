f35df1a0041c2155520a366dfdd76c76fefd3f80
Ali Caner Sezer <alicanerszrr@gmail.com>
2026-09-22 10:36:52 +0300

    test: transaction-service için embedded kafka ile ci uyumluluğu

--- transaction-service/src/test/java/com/banking/transaction/TransactionServiceApplicationTests.java
+++ transaction-service/src/test/java/com/banking/transaction/TransactionServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.banking.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"transaction-events"})
 class TransactionServiceApplicationTests {
 
 	@Test
